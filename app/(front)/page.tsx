485b466893b4307c86235bb6ae987f239c85bda3
bokangleqele <bokangleqele7@gmail.com>
2024-08-06 15:22:56 +0200

    Footer

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,10 +1,12 @@
 import Hero from "@/components/FrontEnd/Hero";
+import Footer from "@/components/FrontEnd/Footer";
 import React from "react";
 
 export default async function Home() {
   return (
     <section className="">
       <Hero /> {/* Rendering Hero component for homepage */}
+      <Footer/>
      </section>
   );
-}
+} 
