3f80c6952c5831a40b3be85f5f2bafb0c7277215
Bridgett-Pampiri <bridgettpampiri01@gmail.com>
2024-08-06 14:59:44 +0200

    Navigation barr

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,9 +1,13 @@
 import Hero from "@/components/FrontEnd/Hero";
+import Navbar from "@/components/FrontEnd/Navbar";
+import Carousel from "@/components/FrontEnd/Carousel"
 import React from "react";
 
 export default async function Home() {
   return (
     <section className="">
+      <Navbar />
+      <Carousel/>
       <Hero /> {/* Rendering Hero component for homepage */}
      </section>
   );
