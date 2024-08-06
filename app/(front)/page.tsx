34dad3fbbf9196d060380e154369017076743a84
Fuma1322 <leonardfuma26@gmail.com>
2024-08-06 11:36:16 +0200

    project initialisation

---
+++ app/(front)/page.tsx
@@ -0,0 +1,10 @@
+import Hero from "@/components/ui/FrontEnd/Hero";
+import React from "react";
+
+export default async function Home() {
+  return (
+    <section className="">
+      <Hero /> {/* Rendering Hero component for homepage */}
+     </section>
+  );
+}
