8db581219ca9307f717f0103d9625c077aeee825
bokangleqele <bokangleqele7@gmail.com>
2024-08-08 15:06:14 +0200

    supposed to be search in opportunities

--- app/(front)/opportunities/page.tsx
+++ app/(front)/opportunities/page.tsx
@@ -1,14 +1,16 @@
 import Hero from "@/components/FrontEnd/Hero";
 import Footer from "@/components/FrontEnd/Footer";
 import Navbar from "@/components/FrontEnd/Navbar";
+
 import React from "react";
+import SearchBar from "@/components/FrontEnd/searchBar"
 
 
 export default async function Opportunities() {
   return (
     <>
     
-     
+    <SearchBar/>
       
      </>
   );
