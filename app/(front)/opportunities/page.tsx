5fd9837d111af696811b1c884d45b409ff3ae6fb
bokangleqele <bokangleqele7@gmail.com>
2024-08-08 15:16:14 +0200

    search

---
+++ app/(front)/opportunities/page.tsx
@@ -0,0 +1,17 @@
+import Hero from "@/components/FrontEnd/Hero";
+import Footer from "@/components/FrontEnd/Footer";
+import Navbar from "@/components/FrontEnd/Navbar";
+
+import React from "react";
+import SearchBar from "@/components/FrontEnd/SearchBar"
+
+
+export default async function Opportunities() {
+  return (
+    <>
+    
+    <SearchBar/>
+      
+     </>
+  );
+} 
