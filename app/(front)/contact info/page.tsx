a919ea0417b8f43045579d59abd051c82cebdaf0
Bridgett-Pampiri <bridgettpampiri01@gmail.com>
2024-08-08 10:41:20 +0200

    Other pages

---
+++ app/(front)/contact info/page.tsx
@@ -0,0 +1,15 @@
+
+import Footer from "@/components/FrontEnd/Footer";
+import Navbar from "@/components/FrontEnd/Navbar";
+import React from "react";
+
+
+export default async function Contact() {
+  return (
+    <section className="">
+      <Navbar />
+      
+      <Footer/>
+     </section>
+  );
+} 
