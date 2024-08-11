0759fe47ed899e1070839d41cdd12d978315c88e
Fuma1322 <leonardfuma26@gmail.com>
2024-08-11 02:35:06 +0200

    search bar fixed and dark mode support added...02:22am

--- app/(front)/page.tsx
+++ app/(front)/page.tsx
@@ -1,6 +1,4 @@
 import Hero from "@/components/FrontEnd/Hero";
-import Footer from "@/components/FrontEnd/Footer";
-import Navbar from "@/components/FrontEnd/Navbar";
 import React from "react";
 
 
