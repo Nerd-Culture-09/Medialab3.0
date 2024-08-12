890e3b8575b35a57439b0fea2ccc62f5266b2b0d
Bridgett-Pampiri <bridgettpampiri01@gmail.com>
2024-08-12 08:47:58 +0200

    Modified contact info page

--- app/(front)/contactInfo/page.tsx
+++ app/(front)/contactInfo/page.tsx
@@ -1,11 +1,16 @@
-
+import Hero from "@/components/FrontEnd/Hero";
+import Footer from "@/components/FrontEnd/Footer";
+import Navbar from "@/components/FrontEnd/Navbar";
 import React from "react";
+import ContactInfo from "@/components/FrontEnd/ContactInfo";
 
 
 export default async function Contact() {
   return (
-   <>
-
-   </>
+    <>
+    
+     <ContactInfo />git 
+      
+     </>
   );
 } 
