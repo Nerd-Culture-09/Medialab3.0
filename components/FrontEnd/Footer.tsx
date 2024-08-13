51e5f504354004eb2bae68540626a2d60df72a77
Bridgett-Pampiri <bridgettpampiri01@gmail.com>
2024-08-13 12:18:36 +0200

    Modified the contact support dialog to change in dark mode

--- components/FrontEnd/Footer.tsx
+++ components/FrontEnd/Footer.tsx
@@ -56,82 +56,83 @@ export default function  Footer()  {
                     </button>
                 </div>
             
+                
                 {isDialogOpen && (
                     <div className="fixed inset-0 flex items-center justify-center bg-black bg-opacity-50 z-50">
-                        <div className="bg-white p-6 rounded-lg w-full  max-w-lg">
-                            <h4 className="text-xl font-bold mb-4">Contact Us</h4>
-                            <form
-                                onSubmit={(e) => e.preventDefault()}
-                                className="space-y-5"
-                            >
-                                <div className="flex flex-col items-center gap-y-5 gap-x-6 [&>*]:w-full sm:flex-row">
-                                    <div>
-                                        <label className="font-medium">First name</label>
-                                        <input
-                                            type="text"
-                                            required
-                                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                                        />
-                                    </div>
-                                    <div>
-                                        <label className="font-medium">Last name</label>
-                                        <input
-                                            type="text"
-                                            required
-                                            className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                                        />
-                                    </div>
-                                </div>
-                                <div>
-                                    <label className="font-medium">Email</label>
-                                    <input
-                                        type="email"
-                                        required
-                                        className="w-full mt-2 px-3 py-2 text-gray-500 bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                                    />
-                                </div>
-                                <div>
-                                    <label className="font-medium">Phone number</label>
-                                    <div className="relative mt-2">
-                                        <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r pr-2">
-                                            <select className="text-sm bg-transparent outline-none rounded-lg h-full">
-                                                <option>US</option>
-                                                <option>SA</option>
-                                                <option>LS</option>
-                                            </select>
-                                        </div>
-                                        <input
-                                            type="number"
-                                            placeholder="+266 5321 3789"
-                                            required
-                                            className="w-full pl-[4.5rem] pr-3 py-2 appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"
-                                        />
-                                    </div>
-                                </div>
-                                <div>
-                                    <label className="font-medium">Message</label>
-                                    <textarea required className="w-full mt-2 h-36 px-3 py-2 resize-none appearance-none bg-transparent outline-none border focus:border-indigo-600 shadow-sm rounded-lg"></textarea>
-                                </div>
-                                
-                                <div className="flex flex-col items-center gap-y-5 gap-x-6 [&>*]:w-full sm:flex-row">
-                                    <button
-                                        onClick={closeDialog}
-                                        className="mt-4 px-4 py-2 bg-red-600 text-white rounded-lg"
-                                    >
-                                        Cancel
-                                    </button>
-                                    <button
-                                        type="submit"
-                                        className="mt-4 px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
-                                    >
-                                        Submit
-                                    </button>
-                                </div>
-                            </form>
-                            
+                    <div className="bg-white dark:bg-gray-800 p-6 rounded-lg w-full max-w-lg">
+                        <h4 className="text-xl font-bold text-gray-900 dark:text-white mb-4">Contact Us</h4>
+                        <form
+                        onSubmit={(e) => e.preventDefault()}
+                        className="space-y-5"
+                        >
+                        <div className="flex flex-col items-center gap-y-5 gap-x-6 [&>*]:w-full sm:flex-row">
+                            <div>
+                            <label className="font-medium text-gray-900 dark:text-gray-200">First name</label>
+                            <input
+                                type="text"
+                                required
+                                className="w-full mt-2 px-3 py-2 text-gray-900 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:border-indigo-600 dark:focus:border-indigo-400 outline-none shadow-sm rounded-lg"
+                            />
+                            </div>
+                            <div>
+                            <label className="font-medium text-gray-900 dark:text-gray-200">Last name</label>
+                            <input
+                                type="text"
+                                required
+                                className="w-full mt-2 px-3 py-2 text-gray-900 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:border-indigo-600 dark:focus:border-indigo-400 outline-none shadow-sm rounded-lg"
+                            />
+                            </div>
                         </div>
+                        <div>
+                            <label className="font-medium text-gray-900 dark:text-gray-200">Email</label>
+                            <input
+                            type="email"
+                            required
+                            className="w-full mt-2 px-3 py-2 text-gray-900 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:border-indigo-600 dark:focus:border-indigo-400 outline-none shadow-sm rounded-lg"
+                            />
+                        </div>
+                        <div>
+                            <label className="font-medium text-gray-900 dark:text-gray-200">Phone number</label>
+                            <div className="relative mt-2">
+                            <div className="absolute inset-y-0 left-3 my-auto h-6 flex items-center border-r border-gray-300 dark:border-gray-600 pr-2">
+                                <select className="text-sm bg-gray-100 dark:bg-gray-700 text-gray-900 dark:text-gray-300 outline-none rounded-lg h-full">
+                                <option>LS</option>
+                                <option>SA</option>
+                                <option>US</option>
+                                </select>
+                            </div>
+                            <input
+                                type="number"
+                                placeholder="+266 5321 3789"
+                                required
+                                className="w-full pl-[4.5rem] pr-3 py-2 text-gray-900 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:border-indigo-600 dark:focus:border-indigo-400 outline-none shadow-sm rounded-lg"
+                            />
+                            </div>
+                        </div>
+                        <div>
+                            <label className="font-medium text-gray-900 dark:text-gray-200">Message</label>
+                            <textarea required className="w-full mt-2 h-36 px-3 py-2 text-gray-900 dark:text-gray-300 bg-gray-100 dark:bg-gray-700 border border-gray-300 dark:border-gray-600 focus:border-indigo-600 dark:focus:border-indigo-400 outline-none resize-none shadow-sm rounded-lg"></textarea>
+                        </div>
+                        
+                        <div className="flex flex-col items-center gap-y-5 gap-x-6 [&>*]:w-full sm:flex-row">
+                            <button
+                            onClick={closeDialog}
+                            className="mt-4 px-4 py-2 bg-red-600 text-white rounded-lg"
+                            >
+                            Cancel
+                            </button>
+                            <button
+                            type="submit"
+                            className="mt-4 px-4 py-2 text-white font-medium bg-indigo-600 hover:bg-indigo-500 active:bg-indigo-600 rounded-lg duration-150"
+                            >
+                            Submit
+                            </button>
+                        </div>
+                        </form>
+                    </div>
                     </div>
                 )}
+  
             </div>
             </div>
             <ul className="items-center justify-center mt-8 space-y-5 sm:flex sm:space-x-4 sm:space-y-0">
