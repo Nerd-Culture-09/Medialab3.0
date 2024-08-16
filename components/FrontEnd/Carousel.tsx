b61504839649633a1d50169d4fbeb453c8dd12e6
bokangleqele <bokangleqele7@gmail.com>
2024-08-16 10:49:04 +0200

    Cover photo added to the home page

--- components/FrontEnd/Carousel.tsx
+++ components/FrontEnd/Carousel.tsx
@@ -3,25 +3,18 @@
 import React from "react";
 
 export function Carousel() {
-  const imageUrl = "/videoeditor.jpg"; 
+  const imageUrl = "/cover.jpg"; 
 
   return (
-    <div className="relative w-full h-[500px] overflow-hidden">
+    <div className="relative w-full h-[20vh] overflow-hidden flex justify-center items-center">
       
       <div 
-        className="absolute inset-0 bg-cover bg-center"
+        className="bg-no-repeat bg-center bg-cover w-full h-full"
         style={{ backgroundImage: `url(${imageUrl})` }}
       >
-        
-        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center p-4">
+      
+        <div className="absolute inset-0 bg-black bg-opacity-50 flex flex-col items-center justify-center p-4">          
           
-          <h2 className="text-white text-center text-xl sm:text-2xl md:text-3xl lg:text-4xl font-bold mb-4">
-            Media Lab
-          </h2>
-          
-          <p className="text-white text-center text-base sm:text-lg md:text-xl lg:text-2xl">
-            Passionate about creating engaging and impactful multimedia experiences.
-          </p>
         </div>
       </div>
     </div>
--- components/FrontEnd/Hero.tsx
+++ components/FrontEnd/Hero.tsx
@@ -13,7 +13,7 @@ export default function Hero() {
     <div className="">
       <div className="relative isolate px-6 pt-14 lg:px-8">
       <div className="lg:px-72 py-10">
-      {/* <Carousel /> */}
+      <Carousel /> 
       <Featured />
       </div>
       {/* <Cards />
