9703edce938d21b5b14f74bf405b9b91fd54c233
Bridgett-Pampiri <bridgettpampiri01@gmail.com>
2024-08-19 15:29:32 +0200

    centered the grids under projects

--- components/FrontEnd/Projects.tsx
+++ components/FrontEnd/Projects.tsx
@@ -12,7 +12,7 @@ export default function Projects() {
             <h3 className="text-2xl md:text-2xl lg:text-6xl font-semibold max-w-7xl mx-auto text-center mt-6 relative z-20 py-6 bg-clip-text text-transparent bg-gradient-to-b from-neutral-800 via-neutral-700 to-neutral-700 dark:from-neutral-800 dark:via-white dark:to-white">
             <Cover>Related Projects</Cover>
             </h3>
-            <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+            <div className="grid grid-cols-2 md:grid-cols-3 gap-4">
                 <div className="grid gap-4">
                     <div className="relative">
                         <a href="https://youtube.com/shorts/B5bJKCzl-9M?si=oQglOStoToLqlrQ1" target="_blank" rel="noopener noreferrer">
